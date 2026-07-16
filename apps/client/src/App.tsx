8a081f8a8a6b94508328880b8eb2e715415c8017
Aditya Verma <vermaaditya457@gmail.com>
2026-07-17 01:36:17 +0530

    feat: connect frontend and backend

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react";
+
 function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    const fetchHealth = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/v1/health");
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch");
+        }
+
+        const data = await response.json();
+
+        setMessage(data.message);
+      } catch (error) {
+        console.error(error);
+        setMessage("Failed to connect to server");
+      }
+    };
+
+    fetchHealth();
+  }, []);
+
   return (
     <main className="min-h-screen flex items-center justify-center bg-black">
-      <h1 className="text-5xl font-bold text-white">
-        RendrAI
-      </h1>
+      <h1 className="text-4xl font-bold text-white">{message}</h1>
     </main>
   );
 }
--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
 import healthRouter from './routes/health.routes.js';
-
+import cors from 'cors';
+import { env } from './config/env.js';
 
 export const app = express();
-app.use('/api/v1', healthRouter);
+app.use(
+  cors({
+    origin: env.CLIENT_ORIGIN,
+    credentials: true,
+  })
+);
+
 
 app.use(express.json());
+app.use('/api/v1', healthRouter);
+
